36d9c30dadacf346356b4b0c93a07a103c3e7fc2
Maycon Santana <maycon_gus2009@hotmail.com>
2022-05-11 20:22:51 -0300

    Navbar

---
+++ frontend/src/components/Navbar/index.tsx
@@ -0,0 +1,24 @@
+import './styles.css';
+
+import { ReactComponent as GithubIcon } from 'assets/img/github.svg';
+
+function Navbar() {
+
+    return (
+        <header>
+            <nav className='container'>
+                <div className='dsmovie-nav-content'>
+                    <h1>DSMovie</h1>
+                    <a href="https://github.com/MayconInforGames2017">
+                        <div className='dsmovie-contact-conteiner'>
+                            <GithubIcon />
+                            <p className='dsmovie-contact-link'>/MayconInforgames</p>
+                        </div>
+                    </a>
+                </div>
+            </nav>
+        </header>
+    );
+}
+
+export default Navbar;
